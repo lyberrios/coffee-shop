ea4dff2cc2f6809f5775ab0432075b26ba93c68e
Ermarly Berrios <lyberryo@gmail.com>
2025-04-19 17:52:05 -0300

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
 function css(done) {
   src('src/scss/app.scss')        // Ruta al archivo SCSS principal
+    .pipe(sourcemaps.init())          // Inicializa sourcemaps
     .pipe(sass().on('error', sass.logError)) // Compilar SCSS
     .pipe(postcss([autoprefixer()]))         // Agrega autoprefixer
+    .pipe(sourcemaps.write('.'))          // Escribe sourcemaps
     .pipe(dest('build/css'));     // Guarda como app.css aquí
-  done();
+    done();
 }
 
 function dev() {
